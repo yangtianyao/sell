d8b64b810ab4414a5d46a7181bc32171607c06be
ytylovejava <2456939181@qq.com>
2018-08-11 18:49:27 +0800

    qwe

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <div class="tab">
+      i am tab
+    </div>
+    <div class="content">
+      i am content
+    </div>
+  </div>
+</template>
+
+<script>
+  import header from './components/header/header'
+
+  export default {
+    component: {
+      'v-header': header
+    }
+  }
+</script>
+
+<style>
+
+</style>
